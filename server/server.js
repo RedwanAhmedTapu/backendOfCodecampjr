a06dbe9061395a77920d7afa068234f919b802fe
RedwanAhmedTapuu <redwan25880@gmail.com>
2023-09-14 12:16:09 +0600

    solverd cors policy

--- server/server.js
+++ server/server.js
@@ -40,16 +40,7 @@ app.use(
     credentials: true,
   })
 );
-app.use((req, res, next) => {
-  // res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://codecampjr.vercel.app/"
-  );
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT,PATCH, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
+
 
 app.use(express.json());
 
