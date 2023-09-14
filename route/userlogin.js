381f813810ea8925ab8c4975066eec2bd940f202
RedwanAhmedTapuu <redwan25880@gmail.com>
2023-09-15 03:30:36 +0600

    cors updated

--- route/userlogin.js
+++ route/userlogin.js
@@ -15,7 +15,7 @@ console.log(req.body);
         bcrypt.compare(password, user.password, (err, isMatch) => {
           if (err) {
             console.error("Error comparing passwords:", err.message);
-             res.status(500).json({ message: "Error comparing passwords" });
+             res.status(404).json({ message: "Error comparing passwords" });
           }
 
           if (isMatch) {
@@ -30,7 +30,7 @@ console.log(req.body);
             res.status(200).json({ user, token });
           } else {
             // Passwords do not match
-            res.status(200).json({message:"not any user"});
+            res.status(404).json({message:"not any user"});
           }
         });
       })
