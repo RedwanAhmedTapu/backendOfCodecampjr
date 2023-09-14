c2a44c327fad7fb27d4256264099a8cfd3dd3008
RedwanAhmedTapuu <redwan25880@gmail.com>
2023-09-14 21:46:59 +0600

    cors again changed

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ require("../db/connection");
 // const urlencodedParser = bodyParser.urlencoded({ extended: false });
 app.use(
   cors({
-    origin: "https://codecampjr.vercel.app",
+    origin: "*",
   })
 );
 app.use(
@@ -44,7 +44,7 @@ app.use((req, res, next) => {
   // res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader(
     "Access-Control-Allow-Origin",
-    "https://codecampjr.vercel.app"
+    "*"
   );
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT,PATCH, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
