57106e30db6f33477fb9930286b8d957cc89bf61
RedwanAhmedTapuu <redwan25880@gmail.com>
2023-09-20 09:40:41 +0600

    updated googleAuthVerification

--- route/googleAuth.verfication.js
+++ route/googleAuth.verfication.js
@@ -23,6 +23,8 @@ const googleAuthverfication = async (req, res) => {
         await User.deleteOne({ email });
       }
       return res.status(400).json({ message: "Invalid verification code" });
+    await User.findOneAndUpdate({ email }, { $set: { isLoggedin: true } });
+
     }
     await User.findOneAndUpdate({ email }, { $set: { isVerified: true } });
 
