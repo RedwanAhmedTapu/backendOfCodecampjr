a2fb4f1bf4ec30ef6b969760c50ed85176e6e9fb
RedwanAhmedTapuu <redwan25880@gmail.com>
2023-09-21 08:19:07 +0600

    updated googleAuth

--- route/googleAuth.js
+++ route/googleAuth.js
@@ -1,26 +1,26 @@
-
 const { User, Verification } = require("../models/register.model");
 const bcrypt = require("bcrypt");
 const randomstring = require("randomstring");
-
-
 
 const googleAuthentication = async (req, res) => {
   console.log(req.body);
   try {
     const { fname, lname, email } = req.body;
-  
+
     const userData = await User.findOne({ email: email })
       .exec()
-      .then((user) => {
+      .then(async (user) => {
         if (user) {
-          res.send({message:"user already exist"});
+          res.send({ message: "user already exist" });
+          await User.findOneAndUpdate(
+            { email },
+            { $set: { isLoggedin: true } }
+          );
         } else {
           const newUser = new User({
             fname,
             lname,
-            email
-          
+            email,
           });
           newUser.save();
 
@@ -30,20 +30,17 @@ const googleAuthentication = async (req, res) => {
             code: verificationCode,
           });
           newVerification.save();
+          await User.findOneAndUpdate(
+            { email },
+            { $set: { isLoggedin: true } }
+          );
 
-        
-
-          res.status(200).json({message:`${verificationCode}`});
-          
+          res.status(200).json({ message: `${verificationCode}` });
         }
-       
-        
       })
       .catch((err) => {
         res.status(404).send("err");
       });
-  await User.findOneAndUpdate({ email }, { $set: { isLoggedin: true } });
-
   } catch (error) {
     res.status(404).send("error");
   }
